78688e95ae1c31a0001737319a58cbbe4de69afc
Anton Lamtev <antonlamtev@gmail.com>
2019-11-20 23:41:13 +0300

    Bin tree almost done

--- Task2_1.hs
+++ Task2_1.hs
@@ -8,29 +8,50 @@ module Task2_1 where
 
 import Todo(todo)
 
+import Prelude hiding (lookup)
+
 -- Ассоциативный массив на основе бинарного дерева поиска
 -- Ключи - Integer, значения - произвольного типа
-data TreeMap v = ChangeMe
+data TreeMap v = EmptyTree
+               | Node{ key::Integer, value::v, lch::TreeMap v, rch::TreeMap v }
+               deriving Show
 
 -- Пустое дерево
 emptyTree :: TreeMap v
-emptyTree = todo
+emptyTree = EmptyTree
 
 -- Содержится ли заданный ключ в дереве?
 contains :: TreeMap v -> Integer -> Bool
-contains t k = todo
+contains EmptyTree _                   = False
+contains (Node tk v l r) k | tk == k   = True
+                           | k < tk    = contains l k
+                           | otherwise = contains r k
 
 -- Значение для заданного ключа
-lookup :: Integer -> TreeMap v -> v
-lookup k t = todo
+lookup :: Integer -> TreeMap v -> Maybe v
+lookup _ EmptyTree                   = Nothing
+lookup k (Node tk v l r) | k == tk   = Just v
+                         | k < tk    = lookup k l
+                         | otherwise = lookup k r
 
 -- Вставка пары (ключ, значение) в дерево
 insert :: (Integer, v) -> TreeMap v -> TreeMap v
-insert (k, v) t = todo
+insert (k, v) EmptyTree                    = Node k v EmptyTree EmptyTree
+insert (k, v) (Node tk tv l r) | k == tk   = Node k v l r
+                               | k < tk    = Node k v (insert (k, v) l) r
+                               | otherwise = Node k v l (insert (k, v) r)
 
 -- Удаление элемента по ключу
 remove :: Integer -> TreeMap v -> TreeMap v
-remove i t = todo
+remove _ EmptyTree = EmptyTree
+remove k (Node tk v l r) | k == tk   = helper l r
+                         | k < tk    = Node tk v (remove k l) r
+                         | otherwise = Node tk v l (remove k r)
+                            where helper l r = case (l, r) of
+                                                  (EmptyTree, EmptyTree) -> EmptyTree
+                                                  (EmptyTree, _)         -> r
+                                                  (_, EmptyTree)         -> l
+                                                  (_, _)                 -> Node (key l) (value l) (lch l) (helper (rch l) r)
 
 -- Поиск ближайшего снизу ключа относительно заданного
 nearestLE :: Integer -> TreeMap v -> (Integer, v)
@@ -38,11 +59,12 @@ nearestLE i t = todo
 
 -- Построение дерева из списка пар
 treeFromList :: [(Integer, v)] -> TreeMap v
-treeFromList lst = todo
+treeFromList lst = foldr insert EmptyTree lst
 
 -- Построение списка пар из дерева
 listFromTree :: TreeMap v -> [(Integer, v)]
-listFromTree t = todo
+listFromTree EmptyTree = []
+listFromTree (Node k v l r) = listFromTree l ++ [(k, v)] ++ listFromTree r
 
 -- Поиск k-той порядковой статистики дерева
 kMean :: Integer -> TreeMap v -> (Integer, v)
